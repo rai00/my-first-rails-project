edcb5c9898e68611fd8a214b39921e2b13e47dea
rai00 <rbpujol@gmail.com>
2016-01-11 04:38:57 +0100

    Added ruby files from c9

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,18 @@
+class WelcomeController < ApplicationController
+  
+  def index
+  end
+  
+  def about
+    $title = "What's about me?"
+    
+    #@var = myFunction(10)
+  end
+  
+  #def myFunction(my_var)  #Function that does whatever you want...
+  #  my_var *= my_var
+  #  return my_var
+  #end
+  
+end
+
